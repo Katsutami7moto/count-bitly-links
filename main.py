cd36aac1cd3a8f9df2f7c6d2e3ac31cea8ebb8bb
Katsutami7moto <nclsf87@gmail.com>
2022-06-16 18:53:06 +0300

    PEP8 refactoring

--- main.py
+++ main.py
@@ -6,33 +6,36 @@ import requests
 from dotenv import load_dotenv
 
 
-def retrieve_user_info(bearer: str, api_url: str) -> dict:
+def retrieve_user_info(bearer: str, api_base: str) -> dict:
     headers = {
         'Authorization': bearer,
     }
     api_method = 'user'
-    response = requests.get(url=api_url.format(api_method), headers=headers)
+    api_url = api_base.format(api_method)
+    response = requests.get(url=api_url, headers=headers)
     response.raise_for_status()
     return response.json()
 
 
-def shorten_link(link: str, group_guid: str, bearer: str, api_url: str) -> str:
+def shorten_link(link: str, bearer: str, api_base: str) -> str:
+    guid: str = retrieve_user_info(bearer, api_base)['default_group_guid']
     headers = {
         'Authorization': bearer,
     }
     payload = {
         'long_url': link,
         'domain': 'bit.ly',
-        'group_guid': group_guid,
+        'group_guid': guid,
     }
     api_method = 'shorten'
-    response = requests.post(url=api_url.format(api_method), headers=headers, json=payload)
+    api_url = api_base.format(api_method)
+    response = requests.post(url=api_url, headers=headers, json=payload)
     response.raise_for_status()
     bitlink: str = response.json()['link']
     return bitlink
 
 
-def count_clicks(link: str, bearer: str, api_url: str) -> int:
+def count_clicks(link: str, bearer: str, api_base: str) -> int:
     headers = {
         'Authorization': bearer,
     }
@@ -42,28 +45,30 @@ def count_clicks(link: str, bearer: str, api_url: str) -> int:
     parsed = urlparse(link)
     parsed_bitlink = f'{parsed.netloc}{parsed.path}'
     api_method = f'bitlinks/{parsed_bitlink}/clicks/summary'
-    response = requests.get(url=api_url.format(api_method), headers=headers, params=params)
+    api_url = api_base.format(api_method)
+    response = requests.get(url=api_url, headers=headers, params=params)
     response.raise_for_status()
     clicks: int = response.json()['total_clicks']
     return clicks
 
 
-def is_bitlink(link: str, bearer: str, api_url: str) -> bool:
+def is_bitlink(link: str, bearer: str, api_base: str) -> bool:
     headers = {
         'Authorization': bearer,
     }
     parsed = urlparse(link)
     parsed_bitlink = f'{parsed.netloc}{parsed.path}'
     api_method = f'bitlinks/{parsed_bitlink}'
-    response = requests.get(url=api_url.format(api_method), headers=headers)
+    api_url = api_base.format(api_method)
+    response = requests.get(url=api_url, headers=headers)
     return response.ok
 
 
 def main():
     description = """
         Bitly URL shortener. 
-
-        Console utility for shortening web links using bit.ly service and counting clicks on shortened links.
+        Console utility for shortening web links using bit.ly service
+        and counting clicks on shortened links.
         """
     parser = argparse.ArgumentParser(
         description=description
@@ -74,29 +79,30 @@ def main():
     load_dotenv()
     access_token: str = os.getenv('ACCESS_TOKEN')
     bearer = f'Bearer {access_token}'
-    api_url = 'https://api-ssl.bitly.com/v4/{}'
-    guid: str = retrieve_user_info(bearer, api_url)['default_group_guid']
+    api_base = 'https://api-ssl.bitly.com/v4/{}'
 
     while True:
         if args.url:
-            url = args.url
+            user_url = args.url
         else:
-            url = input('Enter a link (or just press "Enter" to quit): ')
-        if not url:
+            user_url = input('Enter a link (or just press "Enter" to quit): ')
+        if not user_url:
             break
         try:
-            wrong_url_test = requests.get(url)
+            wrong_url_test = requests.get(user_url)
             wrong_url_test.raise_for_status()
-            if is_bitlink(url, bearer, api_url):
-                message = f'Number of clicks: {count_clicks(url, bearer, api_url)}'
+            api_args = user_url, bearer, api_base
+            if is_bitlink(*api_args):
+                message = f'Number of clicks: {count_clicks(*api_args)}'
             else:
-                message = f'Bitlink: {shorten_link(url, guid, bearer, api_url)}'
+                message = f'Bitlink: {shorten_link(*api_args)}'
         except requests.exceptions.HTTPError as err:
             print('HTTP error:', err)
             print('It is possible that your link contains a typo.')
         except Exception as err:
             print('Other error:', err)
-            print("If you don't know how to avoid this error, please, contact script's author.")
+            print("""If you don't know how to avoid this error,\
+            please, contact script's author.""")
         else:
             print(message)
         finally:
